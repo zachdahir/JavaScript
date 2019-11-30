f7c2b5504cf8050dc44a84b18b7056fdaaa9762e
Zach Dahir <zachdahir@yahoo.com>
2019-11-29 20:27:45 -0600

    Added code attribution

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,10 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Professor Krasso
+; Date: 11-29-19
+; Modified By: Zach Dahir
+; Description: Hello World program
+;===========================================
+*/ 
 console.log("Hello World, my name is Zach")
